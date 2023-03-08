066be8c3b918f356927938cef84fcef281f73b1b
Ali C. Begen <ali.begen@networked.media>
2023-03-08 06:59:01 +0300

    Add files via upload

--- render.py
+++ render.py
@@ -20,3 +20,12 @@ for php_file in glob.glob(PHP_FILES):
     # change the extension to .html
     html_file = os.path.splitext(php_file)[0] + ".html"
     os.system(f"php {php_file} > {BUILD_DIR}/{html_file}")
+
+# Rewrite links in HTML files
+for html_file in glob.glob(os.path.join(BUILD_DIR, "*.html")):
+    with open(html_file, "r") as f:
+        content = f.read()
+    # Change links to other HTML files
+    content = content.replace(".php", ".html")
+    with open(html_file, "w") as f:
+        f.write(content)
